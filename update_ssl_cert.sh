204342f506af5e3033cac90de759b6f4c4475661
Command <maoxs2@gmail.com>
2022-05-12 02:20:35 +0800

    update domain

--- update_ssl_cert.sh
+++ update_ssl_cert.sh
@@ -2,5 +2,6 @@
 # /root/.acme.sh/acme.sh --issue -d saineating.ngx.fi --dns dns_cf
 # /root/.acme.sh/acme.sh --issue -d www.saineating.m00n.top --dns dns_cf
 # /root/.acme.sh/acme.sh --issue -d saineating.m00n.top --dns dns_cf
-/root/.acme.sh/acme.sh --issue -d saineating.c0mm4nd.com --dns dns_cf
-/root/.acme.sh/acme.sh --issue -d saineating-1.c0mm4nd.com --dns dns_cf
+# /root/.acme.sh/acme.sh --issue -d saineating.c0mm4nd.com --dns dns_cf
+# /root/.acme.sh/acme.sh --issue -d saineating-1.c0mm4nd.com --dns dns_cf
+# comment all because new domain saineating.ml (.ml) is not supported on CF API
