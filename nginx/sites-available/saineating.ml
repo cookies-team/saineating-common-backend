a3aea73e8b8b2fd0b1127152ec4f544e25e082cc
Command <maoxs2@gmail.com>
2022-05-20 15:08:40 +0800

    update paths for compatibility

--- nginx/sites-available/saineating.ml
+++ nginx/sites-available/saineating.ml
@@ -33,7 +33,7 @@ server {
                 auth_basic           "saineating maintaince area";
                 auth_basic_user_file /var/www/.htpasswd;
 
-                root /var/www/saineating-iterations/iter2;
+                root /var/www/saineating-iterations/iter3;
                 try_files $uri $uri/ =404;
         }
 
@@ -92,6 +92,59 @@ server {
                 auth_basic_user_file /var/www/.htpasswd;
 
                 root /var/www/saineating-landing/public;
+                try_files $uri $uri/ =404;
+        }
+}
+
+server {
+        listen 80;
+
+        server_name 2.saineating.ml;
+                
+        index index.html index.debain.html index.nginx-debian.html;
+
+        location /api/ {
+                proxy_pass http://127.0.0.1:4000/;
+        }
+
+        location /apiv2/ {
+                proxy_pass http://127.0.0.1:5000/;
+        }
+
+        location /apiv3/ {
+                proxy_pass http://127.0.0.1:6000/;
+        }
+
+        location /latest {
+                return 301 https://$host/iter1;
+        }
+
+        location /iter1 {
+                auth_basic           "saineating maintaince area";
+                auth_basic_user_file /var/www/.htpasswd;
+
+                root /var/www/saineating-iterations/;
+                try_files $uri $uri/ =404;
+        }
+
+        location /design {
+                root /var/www/saineating-iterations/;
+                try_files $uri $uri/ =404;
+        }
+
+        location / {
+                auth_basic           "saineating maintaince area";
+                auth_basic_user_file /var/www/.htpasswd;
+
+                root /var/www/saineating-iterations/iter2;
+                try_files $uri $uri/ =404;
+        }
+
+        location /public {
+                auth_basic           "saineating maintaince area";
+                auth_basic_user_file /var/www/.htpasswd;
+
+                root /var/www/saineating-landing/;
                 try_files $uri $uri/ =404;
         }
 }
